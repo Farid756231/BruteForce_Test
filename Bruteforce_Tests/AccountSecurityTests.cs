873f72b3e4ea2de7a7f631de5c6253e1d1fe9eda
Farid <farid-1234mn@hotmail.com>
2024-08-31 12:59:52 +0200

    skapat test till AccountSecurity

---
+++ Bruteforce_Tests/AccountSecurityTests.cs
@@ -0,0 +1,27 @@
+﻿using BruteForce_Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bruteforce_Tests
+{
+    public class AccountSecurityTests
+    {
+        [Fact]
+        public void RecordFailedAttempt_ShouldBlockAccountAfterMaxAttempts()
+        {
+            // Arrange
+            var security = new AccountSecurity();
+
+            // Act
+            security.RecordFailedAttempt();
+            security.RecordFailedAttempt();
+            security.RecordFailedAttempt();
+
+            // Assert
+            Assert.True(security.IsBlocked);
+        }
+    }
+}
---
+++ Bruteforce_Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Bruteforce_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
