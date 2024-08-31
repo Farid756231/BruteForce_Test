9e8314723d9a035a4e6240ef650f6431134c681e
Farid <farid-1234mn@hotmail.com>
2024-08-31 13:04:30 +0200

    skapat tester till PasswordBruteForce och TowFactorAutentication classer

---
+++ Bruteforce_Tests/PasswordBruteForcerTests.cs
@@ -0,0 +1,25 @@
+﻿using BruteForce_Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bruteforce_Tests
+{
+    public class PasswordBruteForcerTests
+    {
+        [Fact]
+        public void NextGuess_ShouldGenerateCorrectInitialGuess()
+        {
+            // Arrange
+            var bruteForcer = new PasswordBruteForcer(5);
+
+            // Act
+            string guess = bruteForcer.NextGuess();
+
+            // Assert
+            Assert.Equal("aaaaa", guess); // Note: the expected value should be "aaaaa"
+        }
+    }
+}
---
+++ Bruteforce_Tests/TwoFactorAuthenticationTests.cs
@@ -0,0 +1,26 @@
+﻿using BruteForce_Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bruteforce_Tests
+{
+    public class TwoFactorAuthenticationTests
+    {
+        [Fact]
+        public void VerifyCode_ShouldReturnTrueForCorrectCode()
+        {
+            // Arrange
+            var twoFA = new TwoFactorAuthentication("JBSWY3DPEHPK3PXP");
+            string code = twoFA.GenerateCode();
+
+            // Act
+            bool result = twoFA.VerifyCode(code);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
