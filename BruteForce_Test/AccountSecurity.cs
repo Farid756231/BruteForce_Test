ffec8771e7136e9079b4e444939340e9f60cd78c
Farid <farid-1234mn@hotmail.com>
2024-08-31 12:55:43 +0200

    skapat AcoountSecurity,PasswordBruteForce och TowFactorAuthentication class

---
+++ BruteForce_Test/AccountSecurity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForce_Test
+{
+    public class AccountSecurity
+    {
+        private int failedLoginAttempts = 0;
+        private readonly int maxFailedAttempts = 3;
+        private bool isBlocked = false;
+        private DateTime blockUntil = DateTime.MinValue;
+
+        public bool IsBlocked => isBlocked && DateTime.Now < blockUntil;
+
+        public void RecordFailedAttempt()
+        {
+            failedLoginAttempts++;
+            if (failedLoginAttempts >= maxFailedAttempts)
+            {
+                BlockAccount();
+            }
+        }
+
+        public void ResetFailedAttempts()
+        {
+            failedLoginAttempts = 0;
+        }
+
+        private void BlockAccount()
+        {
+            Console.WriteLine("Too many failed attempts. Blocking account for 5 minutes.");
+            isBlocked = true;
+            blockUntil = DateTime.Now.AddMinutes(5);
+            ResetFailedAttempts();
+        }
+    }
+}
---
+++ BruteForce_Test/TwoFactorAuthentication.cs
@@ -0,0 +1,30 @@
+﻿using OtpNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForce_Test
+{
+    public class TwoFactorAuthentication
+    {
+
+        private readonly Totp otp;
+
+        public TwoFactorAuthentication(string userSecretKey)
+        {
+            otp = new Totp(Base32Encoding.ToBytes(userSecretKey));
+        }
+
+        public string GenerateCode()
+        {
+            return otp.ComputeTotp();
+        }
+
+        public bool VerifyCode(string userCode)
+        {
+            return otp.VerifyTotp(userCode, out long timeStepMatched, new VerificationWindow(previous: 2, future: 2));
+        }
+    }
+}
